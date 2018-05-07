09fb7b51e447936a5c5ded82bd6155f13bb61be6
huanJoey <huan.56@qq.com>
2018-05-07 10:51:07 +0800

    Add files via upload
    Iinitialize

---
+++ js/joy.js
@@ -0,0 +1,22 @@
+/**
+ * Created by huanJoy on 2017/11/12.
+ */
+
+/*数*/
+function isNum(num) {
+
+}
+/*整数*/
+function isZ(num) {
+
+}
+/*自然数*/
+function isN(num) {
+    if (isNum(num) && /^\d+$/.test(num)) {
+
+    }
+}
+/*正整数*/
+function isZp(num) {
+
+}
